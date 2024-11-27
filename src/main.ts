90eddab7e473eb591625c384f9e7213f2e4a90e7
Kursat Korkmaz <kursat.korkmaz123@hotmail.com>
2024-11-27 10:02:11 +0300

    [FIX] "module not resolved" error is fixed. This errir thrown because of frida-compile module. This module is problematic so insted I used esbuild to compile the code and tested. Problem is fixed.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import "frida-il2cpp-bridge";
+import UnityGetFunctionHandler from "./core/implementations/UnityGetFunctionHandler";
 
+const handler = new UnityGetFunctionHandler();
+handler.GetImplementation();
 Il2Cpp.perform(() => {
   console.log("Modding operation is started.");
 });
