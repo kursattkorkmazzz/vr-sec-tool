1d08bb0bed3e2f969088b16256adc36338fe142c
gagir <agirgurkan06@gmail.com>
2025-01-15 15:33:17 +0300

    feat: being immortal in game

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,11 @@ import "frida-il2cpp-bridge";
 const URL = require("@frida/url");
 
 import UnityNetworkingPlatform from "./core/platform/UnityNetworkingPlatform";
-
+import GameMechanics from "./core/platform/GameMechanics";
 Il2Cpp.perform(() => {
-  const unityNetworkingPlatform = new UnityNetworkingPlatform();
-  unityNetworkingPlatform.handleFunctions();
-
+  //const unityNetworkingPlatform = new UnityNetworkingPlatform();
+  //unityNetworkingPlatform.handleFunctions();
+  const gameMechanics = new GameMechanics();
+  gameMechanics.handleFunctions();
   ///Il2Cpp.trace(true).classes(UnityWebRequest).and().attach();
 });
