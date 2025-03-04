4aa41e952d7b272beb5438f54cd5ccf7abd58d9c
gagir <agirgurkan06@gmail.com>
2025-03-04 14:54:16 +0300

    feat: unity3d.analitics.internal.WebRequestHelper.CreateWebRequest url changing

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,22 @@ import "frida-il2cpp-bridge";
 const URL = require("@frida/url");
 
 import UnityNetworkingPlatform from "./core/platform/UnityNetworkingPlatform";
+import UnityAnalyticsPlatform from "./core/platform/UnityAnalyticsPlatform";
 import GameMechanics from "./core/platform/GameMechanics";
 import SystemNetwork from "./core/platform/SystemNetworkingPlatform";
 import DLLs from "./core/platform/DLLsPlatform";
+
+
+
 Il2Cpp.perform(() => {
-  const dllS = new DLLs();
-  dllS.handleFunctions();
+const unityAnlyticsPlatform = new UnityAnalyticsPlatform();
+unityAnlyticsPlatform.handleFunctions();
+
+
+
+
+  //const dllS = new DLLs();
+  //dllS.handleFunctions();
   //const systemNetwork = new SystemNetwork();
   //systemNetwork.handleFunctions();
   //const unityNetworkingPlatform = new UnityNetworkingPlatform();
